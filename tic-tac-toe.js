a7f0a69fd06786e2113125fde543fc433c6961c3
Ugo.R <ugorastell.dev@gmail.com>
2022-12-23 00:03:21 +0100

    mode easy

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -13,23 +13,19 @@ cells.forEach(element => {
         if (caseIndex[index] !==0) {
             console.log("Cette case est occupée")
         }else{
-            if (player == selectPlayer) {
+            if (player !== selectPlayer) {
                 element.innerHTML = '<img src="./img/rond.png" width="50" height="50">';
-                caseIndex[index] = 1;
+                
                 player = 0;
-            }else{
-                element.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
-                caseIndex[index] = 2;
-                player = selectPlayer;
             }
+                modeEasy(index,element)
+                console.log(caseIndex)
 
             if (index !== -1) {
                 element.classList.add("lock");
-                console.log(caseIndex);
+                caseIndex[index] = 1;
             }
             win()
-            
-            
         }
     });
 });
@@ -44,19 +40,19 @@ function win() {
     var text = document.createTextNode("Le joueur");
 
     if( (caseIndex[0] == caseIndex[1] && caseIndex[1] == caseIndex[2] && caseIndex[0] !== 0) || 
-        (caseIndex[3] == caseIndex[4] && caseIndex[4] == caseIndex[5] && caseIndex[3] !== 0)||
-        (caseIndex[6] == caseIndex[7] && caseIndex[7] == caseIndex[8] && caseIndex[6] !== 0)||
+        (caseIndex[3] == caseIndex[4] && caseIndex[4] == caseIndex[5] && caseIndex[3] !== 0) ||
+        (caseIndex[6] == caseIndex[7] && caseIndex[7] == caseIndex[8] && caseIndex[6] !== 0) ||
         (caseIndex[0] == caseIndex[4] && caseIndex[4] == caseIndex[8] && caseIndex[0] !== 0) ||
         (caseIndex[2] == caseIndex[4] && caseIndex[4] == caseIndex[6] && caseIndex[2] !== 0) ||
         (caseIndex[0] == caseIndex[3] && caseIndex[3] == caseIndex[6] && caseIndex[0] !== 0) ||
         (caseIndex[1] == caseIndex[4] && caseIndex[4] == caseIndex[7] && caseIndex[1] !== 0) ||
-        (caseIndex[2] == caseIndex[6] && caseIndex[6] == caseIndex[8] && caseIndex[2] !== 0)) {
+        (caseIndex[2] == caseIndex[5] && caseIndex[5] == caseIndex[8] && caseIndex[2] !== 0)) {
 
         for (element in caseIndex) {
             caseIndex[element] = 1;
         }
         
-        if (player !== selectPlayer) {
+        if (player == selectPlayer) {
             var text = document.createTextNode("Le player 1 à gagné");
         }else{
             var text = document.createTextNode("Le player 2 à gagné");
@@ -68,12 +64,23 @@ function win() {
         
         }else{
             var text = document.createTextNode("Égalité");
-            status.appendChild(status);
+            status.appendChild(text);
         }
     }
 }
 
-function modeEasy() {
+function modeEasy(index,element) {
+    
+
+    if (caseIndex[index] == 0) {
+        const iaDebile = Math.floor(Math.random() * 9);
+        console.log(caseIndex[iaDebile])
+        caseIndex[iaDebile] = 2
+        element.innerHTML = '<img src="./img/rond.png" width="50" height="50">';
+        player = selectPlayer;
+    }
+
+    
     
 }
 
