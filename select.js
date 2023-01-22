bb1b8f832651bbfe7f33d10bbda35a28b72f2481
UgoRastell <urastell@normandiewebschool.fr>
2023-01-22 16:20:35 +0100

    amelioration di tic tac toe

---
+++ select.js
@@ -0,0 +1,43 @@
+let difficulty = 0
+let taille = 0
+let tour = 0
+
+function selectTour(tour) {
+
+    if (tour == 1) {
+        localStorage.setItem('Tour', tour);
+    }
+
+    if (tour == 2) {
+        localStorage.setItem('Tour', tour);
+    }
+
+}
+function selectDifficulty(difficulty) {
+
+    if (difficulty == 1) {
+        localStorage.setItem('Difficulty', difficulty);
+    }
+
+    if (difficulty == 2) {
+        localStorage.setItem('Difficulty', difficulty);
+    }
+
+}
+
+function selectTaille(taille) {
+
+    if (taille == 1) {
+        localStorage.setItem('Taille', taille);
+    }else if (taille == 2) {
+        localStorage.setItem('Taille', taille);
+    }else{
+        localStorage.removeItem('Taille');
+    }
+
+}
+
+
+function selectJouer() {
+    document.location.href = "http://127.0.0.1:5500/tic-tac-toe.html";
+}
--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,36 +1,116 @@
-const cells = document.querySelectorAll(".cell");
 
-let selectPlayer = 1
-let player = selectPlayer;
-const caseIndex =  [0,0,0,
-                    0,0,0,
-                    0,0,0]
+let player = 1
+
+
+if (localStorage.getItem('Taille') == 1) {
+    var caseIndex =  [0,0,0,0,
+                      0,0,0,0,
+                      0,0,0,0,
+                      0,0,0,0];
+    const css = `
+    display: grid;
+    grid-template-columns: repeat(4, auto);
+    width: 406px;
+    margin: 50px auto;`;
+    const element = document.querySelector('.game--container');
+
+    element.style.cssText = css;
+    for (let index = 0; index <= 15; index++) {
+        // create a new div element
+        var div = document.createElement("div");
+        div.setAttribute("data-cell-index",index)
+        div.classList.add("cell");
+        
+        document.getElementById("game--container").appendChild(div);
+    }
+    var cells = document.querySelectorAll(".cell");
+}else if(localStorage.getItem('Taille') == 2){
+    var caseIndex =  [0,0,0,0,0,
+                        0,0,0,0,0,
+                        0,0,0,0,0,
+                        0,0,0,0,0,
+                        0,0,0,0,0]
+    const css = `
+    display: grid;
+    grid-template-columns: repeat(5, auto);
+    width: 506px;
+    margin: 50px auto;`;
+    const element = document.querySelector('.game--container');
+    element.style.cssText = css;
+
+    for (let index = 0; index <= 24; index++) {
+        // create a new div element
+        var div = document.createElement("div");
+        div.setAttribute("data-cell-index",index)
+        div.classList.add("cell");
+        
+        document.getElementById("game--container").appendChild(div);
+    }
+    var cells = document.querySelectorAll(".cell");
+}else{
+    var caseIndex =  [0,0,0,
+                      0,0,0,
+                      0,0,0]
+    const css = `
+    display: grid;
+    grid-template-columns: repeat(3, auto);
+    width: 306px;
+    margin: 50px auto;`;
+    const element = document.querySelector('.game--container');
+    element.style.cssText = css;
+
+    for (let index = 0; index <= 8; index++) {
+        // create a new div element
+        var div = document.createElement("div");
+        div.setAttribute("data-cell-index",index)
+        div.classList.add("cell");
+        
+        document.getElementById("game--container").appendChild(div);
+    }
+    var cells = document.querySelectorAll(".cell");
+}
+
+if (localStorage.getItem('Tour') == 2) {
+    modeEasy()
+}
+
 cells.forEach(element => {
     element.addEventListener('click', function (event) {
-
-        const index = element.getAttribute("data-cell-index");
         
+        const index = element.getAttribute("data-cell-index");
+        console.log("test")
         if (caseIndex[index] !==0) {
             console.log("Cette case est occupée")
+            
         }else{
-            if (player == selectPlayer) {
+            if (player == 1) {
                 element.innerHTML = '<img src="./img/rond.png" width="50" height="50">';
                 caseIndex[index] = 1;
-                player = 0;
-                modeEasy()
                 win()
+                player = 2;
+            }
+
+            if (player == 2 && localStorage.getItem('Difficulty') == 2) {
+                console.log("impossible")
+                modeImpossible()
                 player = 1;
+
+            }
+
+            if (player == 2 && localStorage.getItem('Difficulty') == 1) {
+                console.log("easy")
+                modeEasy()
+                player = 1;
+
             }
 
             if (index !== -1) {
                 element.classList.add("lock");
             }
 
-            modeEasy()
             if (index !== -1) {
                 element.classList.add("lock");
             }
-            win()
             
         }
     });
@@ -58,10 +138,10 @@ function win() {
             caseIndex[element] = 1;
         }
         
-        if (player !== selectPlayer) {
+        if (player !== 2) {
             var text = document.createTextNode("Le player 1 à gagné");
         }else{
-            var text = document.createTextNode("Le player 2 à gagné");
+            var text = document.createTextNode("L'ia à gagné");
         }
     
         status.appendChild(text);
@@ -73,13 +153,11 @@ function win() {
             status.appendChild(text);
         }
     }
+
+    
 }
 
 function modeEasy() {
-
-    // Si ce n'est pas au tour du joueur 2, on ne fait rien
-    if (player !== 0) return;
-
     // On convertit la NodeList en tableau
     const cellsArray = Array.from(cells);
 
@@ -98,23 +176,17 @@ function modeEasy() {
     const index = randomCell.getAttribute("data-cell-index");
     caseIndex[index] = 2;
     console.log(caseIndex)
-
-
-    const iaDebile = Math.floor(Math.random() * 9);
+    win()
+    player = 1;
     
-    if(caseIndex[iaDebile] == 0){
-        caseIndex[iaDebile] = 2;
-        console.log(caseIndex)
-    }else{
-        const iaDebile = Math.floor(Math.random() * 9);
-        caseIndex[iaDebile] = 2;
-        console.log(caseIndex)
-    }
+}
 
-    player = selectPlayer;
+
+function modeImpossible() {
+    
 
 }
-    
+
 
 
 
