3882a5dca7620f8d25e63a9632c7bf2c5de0fe62
UgoRastell <urastell@normandiewebschool.fr>
2023-01-31 11:58:23 +0100

    final ?

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -26,10 +26,10 @@ if (localStorage.getItem('Taille') == 1) {
     var cells = document.querySelectorAll(".cell");
 }else if(localStorage.getItem('Taille') == 2){
     var caseIndex =  [0,0,0,0,0,
-                        0,0,0,0,0,
-                        0,0,0,0,0,
-                        0,0,0,0,0,
-                        0,0,0,0,0]
+                      0,0,0,0,0,
+                      0,0,0,0,0,
+                      0,0,0,0,0,
+                      0,0,0,0,0]
     let css = `
     display: grid;
     grid-template-columns: repeat(5, auto);
@@ -155,38 +155,370 @@ function modeEasy() {
 
 function modeImpossible() {
     var status = document.getElementById("game-status");
+    let cellsArray = Array.from(cells);
 
-    for(let i = 0; i <= 5; i += 3) {
-        //ligne horizon 1
-        if(caseIndex[i] === caseIndex[i + 1] && caseIndex[i] === 1) {
-                
-                // On convertit la NodeList en tableau
-                let cellsArray = Array.from(cells);
-
-                // On récupère la liste des cases encore disponibles
-                let availableCells = cellsArray.filter((cell, index) => caseIndex[index] === 0);
-                // On choisit la case 
-                let contreCell = availableCells[i];
-                console.log(contreCell);
-                // On joue sur cette case
-                contreCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
-                let index = contreCell.getAttribute("data-cell-index");
-                caseIndex[index] = 2;
-                console.log(caseIndex)
-
-                // win ou pas
-                if (localStorage.getItem('Taille') == 1) {
-                    win4x4()
-                }else if (localStorage.getItem('Taille') == 2) {
-                    win5x5()
-                }else{
-                    win3x3()
-                }
-                player = 1;
-                return
+    if (caseIndex[0] == caseIndex[3] && caseIndex[3] == 2 && caseIndex[6] !== 1 || caseIndex[6] == caseIndex[3] && caseIndex[3] == 2 && caseIndex[0] !== 1 || caseIndex[0] == caseIndex[6] && caseIndex[6] == 2 && caseIndex[3] !== 1) {
+        //debut win
+        if (caseIndex[0] == caseIndex[6] && caseIndex[6] == 2) {
+            var selectedCell = cellsArray[3]
+        }else if(caseIndex[0] == 2){
+            var selectedCell = cellsArray[6]
+        }else{
+            var selectedCell = cellsArray[0]
         }
-      } 
-    modeEasy()
+        
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[1] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[7] !== 1 || caseIndex[7] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[1] !== 1 || caseIndex[1] == caseIndex[7] && caseIndex[7] == 2 && caseIndex[4] !== 1){
+        
+        if (caseIndex[1] == caseIndex[7] && caseIndex[7] == 2) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[1] == 2){
+            var selectedCell = cellsArray[7]
+        }else{
+            var selectedCell = cellsArray[1]
+        }
+        
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[2] == caseIndex[5] && caseIndex[5] == 2 && caseIndex[8] !== 1 || caseIndex[8] == caseIndex[5] && caseIndex[5] == 2 && caseIndex[2] !== 1 || caseIndex[2] == caseIndex[8] && caseIndex[8] == 2 && caseIndex[5] !== 1){
+       
+        if (caseIndex[2] == caseIndex[8] && caseIndex[8] == 2) {
+            var selectedCell = cellsArray[5]
+        }else if(caseIndex[2] == 2){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[2]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[0] == caseIndex[1] && caseIndex[1] == 2 && caseIndex[2] !== 1 || caseIndex[2] == caseIndex[1] && caseIndex[1] == 2 && caseIndex[0] !== 1 || caseIndex[0] == caseIndex[2] && caseIndex[2] == 2 && caseIndex[1] !== 1){
+        
+        if (caseIndex[0] == caseIndex[2] && caseIndex[2] == 2) {
+            var selectedCell = cellsArray[1]
+        }else if(caseIndex[0] == 2){
+            var selectedCell = cellsArray[2]
+        }else{
+            var selectedCell = cellsArray[0]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[3] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[5] !== 1 || caseIndex[5] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[3] !== 1 || caseIndex[3] == caseIndex[5] && caseIndex[5] == 2 && caseIndex[4] !== 1){
+        
+        if (caseIndex[3] == caseIndex[5] && caseIndex[5] == 2) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[3] == 2){
+            var selectedCell = cellsArray[5]
+        }else{
+            var selectedCell = cellsArray[3]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[6] == caseIndex[7] && caseIndex[7] == 2 && caseIndex[8] !== 1 || caseIndex[8] == caseIndex[7] && caseIndex[7] == 2 && caseIndex[6] !== 1 || caseIndex[6] == caseIndex[8] && caseIndex[8] == 2 && caseIndex[7] !== 1){
+        
+        if (caseIndex[6] == caseIndex[8] && caseIndex[8] == 2) {
+            var selectedCell = cellsArray[7]
+        }else if(caseIndex[6] == 2){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[6]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[0] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[8] !== 1 || caseIndex[8] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[0] !== 1 || caseIndex[0] == caseIndex[8] && caseIndex[8] == 2 && caseIndex[4] !== 1){
+        
+        if (caseIndex[0] == caseIndex[8] && caseIndex[8] == 2) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[0] == 2){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[0]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[2] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[6] !== 1 || caseIndex[6] == caseIndex[4] && caseIndex[4] == 2 && caseIndex[2] !== 1 || caseIndex[2] == caseIndex[6] && caseIndex[6] == 2 && caseIndex[4] !== 1){
+        
+        if (caseIndex[2] == caseIndex[6] && caseIndex[6] == 2) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[2] == 2){
+            var selectedCell = cellsArray[6]
+        }else{
+            var selectedCell = cellsArray[2]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[0] == caseIndex[3] && caseIndex[3] == 1 && caseIndex[6] !== 2 || caseIndex[6] == caseIndex[3] && caseIndex[3] == 1 && caseIndex[0] !== 2 || caseIndex[0] == caseIndex[6] && caseIndex[6] == 1 && caseIndex[3] !== 2) {
+        // debut defense
+        if (caseIndex[0] == caseIndex[6] && caseIndex[6] == 1) {
+            var selectedCell = cellsArray[3]
+        }else if(caseIndex[0] == 1){
+            var selectedCell = cellsArray[6]
+        }else{
+            var selectedCell = cellsArray[0]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[1] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[7] !== 2 || caseIndex[7] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[1] !== 2 || caseIndex[1] == caseIndex[7] && caseIndex[7] == 1 && caseIndex[4] !== 2){
+ 
+        if (caseIndex[1] == caseIndex[7] && caseIndex[7] == 1) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[1] == 1){
+            var selectedCell = cellsArray[7]
+        }else{
+            var selectedCell = cellsArray[1]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[2] == caseIndex[5] && caseIndex[5] == 1 && caseIndex[8] !== 2 || caseIndex[8] == caseIndex[5] && caseIndex[5] == 1 && caseIndex[2] !== 2 || caseIndex[2] == caseIndex[8] && caseIndex[8] == 1 && caseIndex[5] !== 2){
+       
+        if (caseIndex[2] == caseIndex[8] && caseIndex[8] == 1) {
+            var selectedCell = cellsArray[5]
+        }else if(caseIndex[2] == 1){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[2]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[0] == caseIndex[1] && caseIndex[1] == 1 && caseIndex[2] !== 2 || caseIndex[2] == caseIndex[1] && caseIndex[1] == 1 && caseIndex[0] !== 2 || caseIndex[0] == caseIndex[2] && caseIndex[2] == 1 && caseIndex[1] !== 2){
+        
+        if (caseIndex[0] == caseIndex[2] && caseIndex[2] == 1) {
+            var selectedCell = cellsArray[1]
+        }else if(caseIndex[0] == 1){
+            var selectedCell = cellsArray[2]
+        }else{
+            var selectedCell = cellsArray[0]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[3] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[5] !== 2 || caseIndex[5] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[3] !== 2 || caseIndex[3] == caseIndex[5] && caseIndex[5] == 1 && caseIndex[4] !== 2){
+        
+        if (caseIndex[3] == caseIndex[5] && caseIndex[5] == 1) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[3] == 1){
+            var selectedCell = cellsArray[5]
+        }else{
+            var selectedCell = cellsArray[3]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[6] == caseIndex[7] && caseIndex[7] == 1 && caseIndex[8] !== 2 || caseIndex[8] == caseIndex[7] && caseIndex[7] == 1 && caseIndex[6] !== 2 || caseIndex[6] == caseIndex[8] && caseIndex[8] == 1 && caseIndex[7] !== 2){
+        
+        if (caseIndex[6] == caseIndex[8] && caseIndex[8] == 1) {
+            var selectedCell = cellsArray[7]
+        }else if(caseIndex[6] == 1){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[6]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[0] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[8] !== 2 || caseIndex[8] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[0] !== 2 || caseIndex[0] == caseIndex[8] && caseIndex[8] == 1 && caseIndex[4] !== 2){
+        
+        if (caseIndex[0] == caseIndex[8] && caseIndex[8] == 1) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[0] == 1){
+            var selectedCell = cellsArray[8]
+        }else{
+            var selectedCell = cellsArray[0]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else if(caseIndex[2] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[6] !== 2 || caseIndex[6] == caseIndex[4] && caseIndex[4] == 1 && caseIndex[2] !== 2 || caseIndex[2] == caseIndex[6] && caseIndex[6] == 1 && caseIndex[4] !== 2){
+        
+        if (caseIndex[2] == caseIndex[6] && caseIndex[6] == 1) {
+            var selectedCell = cellsArray[4]
+        }else if(caseIndex[2] == 1){
+            var selectedCell = cellsArray[6]
+        }else{
+            var selectedCell = cellsArray[2]
+        }
+
+        selectedCell.innerHTML = '<img src="./img/croix.png" width="50" height="50">';
+        let index = selectedCell.getAttribute("data-cell-index");
+        caseIndex[index] = 2;
+        console.log(caseIndex)
+        if (localStorage.getItem('Taille') == 1) {
+            win4x4()
+        }else if (localStorage.getItem('Taille') == 2) {
+            win5x5()
+        }else{
+            win3x3()
+        }
+        player = 1;
+    }else{
+        modeEasy()
+    }
+
+    
+
+   
+   
+
+    
 }
 
 
